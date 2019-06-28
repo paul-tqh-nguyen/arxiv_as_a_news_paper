3afff2f50770549d2b60aae634834d4c82cb16dd
paul-tqh-nguyen <paul.tqh.nguyen@gmail.com>
2019-06-28 13:34:15 -0500

    #10 Fix type inconsistency in Author Info Extraction
    We pass around a list of dicts of the form { AUTHOR : AUTHOR_LINK }.
    
    When transforming this list of dicts into JSON as part of our ETL, we used to have a type conflict where we'd expect single dicts in _dwim_author_to_author_link_dictionary_wrt_mirror_link.
    
    This led to our ETL not working.
    
    This patch refactors _dwim_author_to_author_link_dictionary_wrt_mirror_link into _dwim_author_to_author_link_dictionaries_wrt_mirror_link (it does what it says it does, which is fix this issue entirely).

--- utilities/extract_transform_utilities.py
+++ utilities/extract_transform_utilities.py
@@ -175,21 +175,24 @@ def _dwim_arxiv_mirror_link_to_main_site_link(link):
     dwimmed_link = dwimmed_link.replace(ARXIV_URL_INDIA_MIRROR, ARXIV_URL)
     return dwimmed_link
 
-def _dwim_author_to_author_link_dictionary_wrt_mirror_link(author_to_author_link_dictionary):
-    for author in author_to_author_link_dictionary.keys():
-        author_link = author_to_author_link_dictionary[author]
-        dwimmed_author_link = _dwim_arxiv_mirror_link_to_main_site_link(author_link)
-        author_to_author_link_dictionary[author] = dwimmed_author_link
-    return author_to_author_link_dictionary
+def _dwim_author_to_author_link_dictionaries_wrt_mirror_link(author_to_author_link_dictionaries):
+    dwimmed_author_to_author_link_dictionaries = []
+    for author_to_author_link_dictionary in author_to_author_link_dictionaries:
+        for author in author_to_author_link_dictionary.keys():
+            author_link = author_to_author_link_dictionary[author]
+            dwimmed_author_link = _dwim_arxiv_mirror_link_to_main_site_link(author_link)
+            author_to_author_link_dictionary[author] = dwimmed_author_link
+        dwimmed_author_to_author_link_dictionaries.append(author_to_author_link_dictionary)
+    return dwimmed_author_to_author_link_dictionaries
 
 def _recent_page_url_info_tuple_to_dict(info_tuple):
-    link_to_paper_page, title, author_to_author_link_dictionary, primary_subject, secondary_subjects_iterator, abstract = info_tuple
+    link_to_paper_page, title, author_to_author_link_dictionaries, primary_subject, secondary_subjects_iterator, abstract = info_tuple
     secondary_subjects = secondary_subjects_iterator
     link_to_paper_page = _dwim_arxiv_mirror_link_to_main_site_link(link_to_paper_page)
-    author_to_author_link_dictionary = _dwim_author_to_author_link_dictionary_wrt_mirror_link(author_to_author_link_dictionary)
+    author_to_author_link_dictionaries = _dwim_author_to_author_link_dictionaries_wrt_mirror_link(author_to_author_link_dictionaries)
     json_dict = {"page_link" : link_to_paper_page,
                  "research_paper_title" : title,
-                 "author_info" : author_to_author_link_dictionary, 
+                 "author_info" : author_to_author_link_dictionaries, 
                  "primary_subject" : primary_subject, 
                  "secondary_subjects" : secondary_subjects,
                  "abstract" : abstract}
