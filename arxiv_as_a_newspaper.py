c51ff0d26c8a9887a475d7d818a14213d5e7618b
paul-tqh-nguyen <paul.tqh.nguyen@gmail.com>
2019-06-29 13:06:06 -0500

    #12 Fix ordering issue in front end server initialization
    This patch fixes a bug introduced by ancillary refactoring in previous patches.
    
    We were attempting to start our server prior to installing the necessary libraries.
    
    That made it impossible to start the front end server on a fresh check out.

--- arxiv_as_a_newspaper.py
+++ arxiv_as_a_newspaper.py
@@ -46,9 +46,9 @@ def start_front_end_server():
     print()
     try:
         print("Installing libraries necessary for front end...")
-        subprocess.check_call("cd front_end/ && npm start", shell=True)
+        subprocess.check_call("cd front_end/ && npm install", shell=Tru
         print("Starting front end server...")
-        subprocess.check_call("cd front_end/ && npm install", shell=True)
+        subprocess.check_call("cd front_end/ && npm start", shell=True)e)
     except KeyboardInterrupt as err:
         print("\n\n")
         print("Exiting front end interface.")
