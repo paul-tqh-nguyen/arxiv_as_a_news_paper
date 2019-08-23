cb6326599a27790c9e73f68131e834730ddbb178
paul-tqh-nguyen <paul.tqh.nguyen@gmail.com>
2019-08-23 15:32:42 -0500

    #53 Stop rendering loading screen after loading completes.
    It was pointed out in #53 that the loading screen was waiting around and still being rendered despite the fact that it wasn't being used or seen.
    
    This patch makes it so that the loading screen completes at least one iteration of animation before no longer being rendered.

--- front_end/src/Components/LoadingScreen.js
+++ front_end/src/Components/LoadingScreen.js
@@ -5,26 +5,49 @@ const loadingBarAnimationDurationInSeconds = 4;
 const numberOfLoadingBars = 10;
 
 export class LoadingScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loadingScreenWorthShowing: true,
+            shutOffDelayer: null,
+        };
+    }
+
+    componentWillUnmount() {
+        let { shutOffDelayer } = this.state;
+        clearInterval(shutOffDelayer);
+    }
+
     render() {
         let { dataIsLoaded } = this.props;
-        let loadingScreenClass = dataIsLoaded ? 'loading-screen loading-finished' : 'loading-screen' ;
-        let loadingBars = [];
-        for(var loadingBarIndex = 0; loadingBarIndex<numberOfLoadingBars; loadingBarIndex++) {
-            let loadingBarWaitTime = loadingBarIndex * loadingBarAnimationDurationInSeconds / numberOfLoadingBars;
-            let style = {width: `100%`,
-                         height: `100%`,
-                         paddingTop: '100%',
-                         animation : `loading-screen-animation ${parseInt(loadingBarAnimationDurationInSeconds)}s ease-in-out`,
-                         animationDelay: `${loadingBarWaitTime}s`,
-                         color: 'white',
-		         fontSize: '4rem',
-                         fontFamily: 'Libre Baskerville',
-	                 textAlign: 'center',
-                        };
-            loadingBars.push(<div style={style} key={`Loading Bar ${loadingBarIndex}`}></div>);
+        let { loadingScreenWorthShowing, shutOffDelayer } = this.state;
+        let renderedComponent = null; 
+        if ( loadingScreenWorthShowing ) {
+            let loadingScreenClass = dataIsLoaded ? 'loading-screen loading-finished' : 'loading-screen' ;
+            if ( dataIsLoaded && ! shutOffDelayer ) {
+                let shutOffFunction = () => { this.setState({ loadingScreenWorthShowing: false }); };
+                let shutOffDelayTime = 6000;
+                this.setState({
+                    shutOffDelayer: setTimeout( shutOffFunction, shutOffDelayTime),
+                });
+            }
+            let loadingBars = [];
+            for(var loadingBarIndex = 0; loadingBarIndex<numberOfLoadingBars; loadingBarIndex++) {
+                let loadingBarWaitTime = loadingBarIndex * loadingBarAnimationDurationInSeconds / numberOfLoadingBars;
+                let style = {width: `100%`,
+                             height: `100%`,
+                             paddingTop: '100%',
+                             animation : `loading-screen-animation ${parseInt(loadingBarAnimationDurationInSeconds)}s ease-in-out`,
+                             animationDelay: `${loadingBarWaitTime}s`,
+                             color: 'white',
+		             fontSize: '4rem',
+                             fontFamily: 'Libre Baskerville',
+	                     textAlign: 'center',
+                            };
+                loadingBars.push(<div style={style} key={`Loading Bar ${loadingBarIndex}`}></div>);
+            };
+            renderedComponent = <div className={loadingScreenClass}>{loadingBars}</div>;
         }
-        return (
-            <div className={loadingScreenClass}>{loadingBars}</div>
-        );
+        return renderedComponent;
     }
 }
