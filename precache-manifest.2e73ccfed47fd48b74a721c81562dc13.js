0fc017d162cc1efb8193f6cc0e982dac8186e9ec
paul-tqh-nguyen <paul.tqh.nguyen@gmail.com>
2019-08-23 11:00:09 -0500

    Updates

---
+++ precache-manifest.2e73ccfed47fd48b74a721c81562dc13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2770fc0f0aad5aed604f2947933349e",
+    "url": "/arxiv_as_a_newspaper/index.html"
+  },
+  {
+    "revision": "5b6ec0374969f445366c",
+    "url": "/arxiv_as_a_newspaper/static/css/main.078745b6.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/arxiv_as_a_newspaper/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "5b6ec0374969f445366c",
+    "url": "/arxiv_as_a_newspaper/static/js/main.19e0c645.chunk.js"
+  },
+  {
+    "revision": "4f4fcb19c519a9a981c1",
+    "url": "/arxiv_as_a_newspaper/static/js/runtime~main.d44947d7.js"
+  }
+]);
