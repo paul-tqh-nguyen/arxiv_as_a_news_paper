e07a571386c7ba9194acad7b475692b40d2f4107
paul-tqh-nguyen <paul.tqh.nguyen@gmail.com>
2020-01-18 15:42:06 -0600

    Updates

--- precache-manifest.4614e67632e161a17269b5f9c0f0f8e5.js
+++ precache-manifest.4614e67632e161a17269b5f9c0f0f8e5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2a385824a069a05ed5e408dfb82d7786",
+    "revision": "99c595364ce720cbebebfbc272d17edb",
     "url": "/arxiv_as_a_newspaper/index.html"
   },
   {
-    "revision": "0afb65f8a6e8a9ddb098",
+    "revision": "2a95b99b9efea1f70670",
     "url": "/arxiv_as_a_newspaper/static/css/main.b2602035.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/arxiv_as_a_newspaper/static/js/2.2668f2aa.chunk.js"
   },
   {
-    "revision": "0afb65f8a6e8a9ddb098",
-    "url": "/arxiv_as_a_newspaper/static/js/main.cf2f4a22.chunk.js"
+    "revision": "2a95b99b9efea1f70670",
+    "url": "/arxiv_as_a_newspaper/static/js/main.f0be7086.chunk.js"
   },
   {
     "revision": "4f4fcb19c519a9a981c1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/arxiv_as_a_newspaper/precache-manifest.672cbd3518850ce605bff0b655a68539.js"
+  "/arxiv_as_a_newspaper/precache-manifest.4614e67632e161a17269b5f9c0f0f8e5.js"
 );
 
 self.addEventListener('message', (event) => {
